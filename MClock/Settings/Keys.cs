1763f9c7890e29dfd87a9a94b642588f1ebbce2a
jakeahumphreys <jakehumphreys@outlook.com>
2024-02-21 22:13:51 +0000

    Adding some consts so that I can remove magic strings from appsettings

---
+++ MClock/Settings/Keys.cs
@@ -0,0 +1,22 @@
+﻿namespace MClock.Settings;
+
+public static class SectionKeys
+{
+    public const string DISCORD_SETTINGS = "DiscordRichPresenceSettings";
+    public const string COLOUR_SETTINGS = "ColourSettings";
+    public const string TIME_SETTINGS = "TimeSettings";
+}
+
+public static class SettingKeys
+{
+    public const string WORK_START_TIME = "WorkStartTime";
+    public const string WORK_END_TIME = "WorkEndTime";
+    public const string LUNCH_START_TIME = "LunchStartTime";
+    public const string LUNCH_END_TIME = "LunchEndTime";
+    public const string KAIZEN_START_TIME = "KaizenStartTime";
+    public const string DEMOS_START_TIME = "DemosStartTime";
+    public const string DEMOS_END_TIME = "DemosEndTime";
+
+    public const string ENABLE_RICH_PRESENCE = "EnableRichPresence";
+    public const string ENABLED_ON_WEEKENDS = "EnabledOnWeekends";
+}
